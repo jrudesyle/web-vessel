2ad2dc4e37d56d6c6087be877c59dbb00eb3c2bd
Jeff Rudesyle <jeff.rudesyle@realtimemed.com>
2026-05-12 16:25:19 -0400

    Deploy: fix icon and service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
 const CACHE_NAME = 'web-vessel-v1';
 const urlsToCache = [
-  '/',
-  '/custom.css',
-  '/inject.js'
+  '/web-vessel/',
+  '/web-vessel/app.html',
+  '/web-vessel/apps.js',
+  '/web-vessel/inject.js',
+  '/web-vessel/themes/matrix.css',
+  '/web-vessel/themes/dark.css',
+  '/web-vessel/themes/discord.css',
+  '/web-vessel/themes/notion.css',
+  '/web-vessel/themes/gmail.css',
+  '/web-vessel/themes/default.css'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache error:', err))
   );
 });
 
@@ -16,5 +24,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(() => fetch(event.request))
   );
 });
